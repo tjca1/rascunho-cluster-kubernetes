6cc85cfe3605964ebba7659c80468514cc3e03af
Thiago Araujo <tjca1@hotmail.com>
2019-03-27 15:53:53 -0300

    1o Commit

---
+++ backend-scm/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const github = require("./api/github");
+const allowCors = require('./config/cors')
+
+const app = express();
+app.use(allowCors);
+const apiPrefix = "/api/scm"
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(`${apiPrefix}/github`, github);
+app.get(`${apiPrefix}/health`, (req, res) => res.json({ msg: "Health OK" }));
+
+const port = process.env.PORT || 3030;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ backend-user/config/cors.js
@@ -0,0 +1,10 @@
+
+var uuid = require('node-uuid');
+
+module.exports = function(req, res, next) {
+    req.id = uuid.v4()
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization')
+    next()
+}
